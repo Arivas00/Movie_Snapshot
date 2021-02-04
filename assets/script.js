3facf58395fa7b1d9ef148d4395f84d849dbaa8d
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-02-03 20:33:59 -0500

    On click for search button added

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
 // Javascript
+
+
+//OMDB key
+var omdbData = "http://www.omdbapi.com/?apikey=7af33c3a&"
+var omdbPoster = "http://img.omdbapi.com/?apikey=7af33c3a&"
+var search = "s=" 
+
+
+
+var searchValue = $("#searchBar").val()
+
+$("#searchBtn").on('click', function (e) {
+    e.preventDefault()
+    window.location.href = "results.html"
+    fetch (omdbData + search + searchValue)
+})
