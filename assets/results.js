4652f3da871841f664d4e399255c2e424f212a14
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-02-08 19:05:56 -0500

    tried fixing deployed site

--- assets/results.js
+++ assets/results.js
@@ -1,26 +1,47 @@
 $(document).ready(function () {
+    //Youtube API key
+    //var youtubeAPIkey = "AIzaSyBLBL6GzSvQz5PJ_gEJ9zUR0qWMn-XiIgM"
 
-    var youtubeAPIkey = "AIzaSyBLBL6GzSvQz5PJ_gEJ9zUR0qWMn-XiIgM"
+    //OMDB API key
+    var omdbData = "http://www.omdbapi.com/?apikey=7af33c3a&"
 
+    var search = "t="
+    var key = "searchBar"
+    var movie = localStorage.getItem(key)
     var omdbObject = JSON.parse(localStorage.getItem("results"));
+
     $("#movie-search").text(omdbObject.Title)
-    $("#year").text("Year: " + omdbObject.Year) 
+    $("#year").text("Year: " + omdbObject.Year)
     $("#director").text("Director: " + omdbObject.Director)
-    $("#actors").text("Actors: " +omdbObject.Actors)
+    $("#actors").text("Actors: " + omdbObject.Actors)
     $("#genre").text("Genre: " + omdbObject.Genre)
     $("#score").text("Rotten Tomatoes: " + omdbObject.Ratings[1].Value)
     $("#poster").attr("src", omdbObject.Poster)
-    $("#ytplayer").attr("src", )
+    $("#ytplayer").attr("src",)
 
-    
-    fetch(omdbData + search + youtubeAPIkey)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data);
-                    localStorage.setItem("results", JSON.stringify(data))
-                    window.location.href = "results.html"
-                })
+    //Youtube API fetch
+    // fetch(omdbData + search + youtubeAPIkey)
+    //     .then(function (response) {
+    //         return response.json();
+    //     })
+    //     .then(function (data) {
+    //         console.log(data);
+    //         localStorage.setItem("results", JSON.stringify(data))
+    //     })
+
+    // OMDB API Fetch
+    var OMDBFetch = function () {
+        fetch(omdbData + search + movie)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                //console.log(data);
+                localStorage.setItem("results", JSON.stringify(data))
+            })
+
+    }
+
+    OMDBFetch()
 
 });
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
 // Javascript
 $(document).ready(function () {
 
-    //OMDB key
-    var omdbData = "http://www.omdbapi.com/?apikey=7af33c3a&"
-    var search = "t="
     var key = "searchBar"
 
     //Search button on click event listener
@@ -11,28 +8,9 @@ $(document).ready(function () {
         e.preventDefault()
         var searchValue = $("#searchBar").val()
         localStorage.setItem(key, searchValue)
-        var movie = localStorage.getItem(key)
-
-
-        var OMDBFetch = function () {
-            fetch(omdbData + search + movie)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data);
-                    localStorage.setItem("results", JSON.stringify(data))
-                    window.location.href = "results.html"
-                })
-
-        }
-        OMDBFetch()
+        
+        window.location.href = "results.html"
 
     })
-    //you tube API
-
-    //media box JS
-
-    //$('.materialboxed').materialbox();
 });
 
