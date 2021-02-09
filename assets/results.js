d561cc63bbf6e4705483a2856f015a8cb55ca380
Shannon Alexander <shannonmariealexander94@gmail.com>
2021-02-08 20:53:54 -0500

    meger with main up to date

--- assets/results.js
+++ assets/results.js
@@ -1,67 +1,68 @@
 $(document).ready(function () {
-    //Youtube API key
-    //var youtubeAPIkey = "AIzaSyBLBL6GzSvQz5PJ_gEJ9zUR0qWMn-XiIgM"
+  //Youtube API key
+  //var youtubeAPIkey = "AIzaSyBLBL6GzSvQz5PJ_gEJ9zUR0qWMn-XiIgM"
 
-    //OMDB API key
-    var omdbData = "https://www.omdbapi.com/?apikey=7af33c3a&"
+  //OMDB API key
+  var omdbData = "https://www.omdbapi.com/?apikey=7af33c3a&"
 
-    var search = "t="
-    var key = "searchBar"
-    var movie = localStorage.getItem(key)
+  var search = "t="
+  var key = "searchBar"
+  var movie = localStorage.getItem(key)
 
-    //Youtube API fetch
-    // fetch(omdbData + search + youtubeAPIkey)
-    //     .then(function (response) {
-    //         return response.json();
-    //     })
-    //     .then(function (data) {
-    //         console.log(data);
-    //         localStorage.setItem("results", JSON.stringify(data))
-    //     })
+  //Youtube API fetch
+  // fetch(omdbData + search + youtubeAPIkey)
+  //     .then(function (response) {
+  //         return response.json();
+  //     })
+  //     .then(function (data) {
+  //         console.log(data);
+  //         localStorage.setItem("results", JSON.stringify(data))
+  //     })
 
-    
-    // OMDB API Fetch
-    var OMDBFetch = function () {
-        fetch(omdbData + search + movie)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                //console.log(data);
-                localStorage.setItem("results", JSON.stringify(data))
-                var omdbObject = JSON.parse(localStorage.getItem("results"));
+  
+  // OMDB API Fetch
+  var OMDBFetch = function () {
+      fetch(omdbData + search + movie)
+          .then(function (response) {
+              return response.json();
+          })
+          .then(function (data) {
+              //console.log(data);
+              localStorage.setItem("results", JSON.stringify(data))
+              var omdbObject = JSON.parse(localStorage.getItem("results"));
 
-                $("#movie-search").text(omdbObject.Title)
-                $("#year").text("Year: " + omdbObject.Year)
-                $("#director").text("Director: " + omdbObject.Director)
-                $("#actors").text("Actors: " + omdbObject.Actors)
-                $("#genre").text("Genre: " + omdbObject.Genre)
-                $("#score").text("Rotten Tomatoes: " + omdbObject.Ratings[1].Value)
-                $("#poster").attr("src", omdbObject.Poster)
-            })
-    }
+              $("#movie-search").text(omdbObject.Title)
+              $("#year").text("Year: " + omdbObject.Year)
+              $("#director").text("Director: " + omdbObject.Director)
+              $("#actors").text("Actors: " + omdbObject.Actors)
+              $("#genre").text("Genre: " + omdbObject.Genre)
+              $("#score").text("Rotten Tomatoes: " + omdbObject.Ratings[1].Value)
+              $("#poster").attr("src", omdbObject.Poster)
+          })
+  }
 
-    OMDBFetch()
-    var youtube = function () {
-        fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet%20&q={}&videoDuration=any&key=AIzaSyCMvE-VUEEYNHxbUoztY10VOOcSJYDCG90")
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (result) {
-                console.log(result)
-                document.getElementById("ytplayer").innerHTML=` 
-                <div class="row">
-                <div class="col s12 m5" id="video">
-                  <div class="VIDEO">
-                    <iframe id="ytplayer" type="text/html" width="640" height="360"
-                      src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
-                      frameborder="0"></iframe>
-                    </span>
-                  </div>
+  OMDBFetch()
+  var youtube = function () {
+      fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet%20&q={}&videoDuration=any&key=AIzaSyCMvE-VUEEYNHxbUoztY10VOOcSJYDCG90")
+          .then(function (response) {
+              return response.json();
+          })
+          .then(function (result) {
+              console.log(result)
+              document.getElementById("ytplayer").innerHTML=` 
+              <div class="row">
+              <div class="col s12 m5" id="video">
+                <div class="VIDEO">
+                  <iframe id="ytplayer" type="text/html" width="640" height="360"
+                    src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+                    frameborder="0"></iframe>
+                  </span>
                 </div>
               </div>
-            </div>`
-            });
-        }
-        youtube()
+            </div>
+          </div>`
+          });
+      }
+      youtube()
 });
+
